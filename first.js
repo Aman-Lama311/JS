5f84320a17031e96ac44c3a4d7ca5d419a768b29
Aman-Lama311 <lamaaman2036@gmail.com>
2024-11-12 16:13:53 +0545

    Add files via upload

---
+++ first.js
@@ -0,0 +1,94 @@
+//loops in js
+// for(let count = 1; count <= 100; count++) {
+//     console.log("Sangita Magar");
+// }
+
+// console.log("Loop has ended")
+// let sum = 0;
+// for(let i = 1; i <= 10; i++) {
+//     sum = sum + i;
+// }
+    
+// console.log("sum =", sum);
+// console.log("Loop has ended")
+
+//infinite loops //Do not write this loops
+
+//while loops
+
+// let i = 1;
+// while (i <= 15) {
+//     console.log("Sangita Magar");
+//     i++;
+// }
+
+//for-of loops
+
+// let str = "JavaScript";
+// let size = 0
+// for (let val of str) {
+//     console.log("val =", val);
+//     size++;
+// }
+
+// console.log("str size =", size);
+
+//for-in loops
+
+// let student = {
+//     Name: "Sangita Magar",
+//     age: 22,
+//     gpa: 3.4,
+//     ispass: true
+// };
+
+// for (let key in student){
+//     console.log("key =", key, "value =", student[key]);
+// }
+
+//practice Q1
+// for(let num = 0; num <= 100; num++) {
+//     if(num % 2 === 0) {
+//         //even num
+//         console.log("num =", num);
+//     }
+// };
+
+//practice Q2
+// let gameNum = 10;
+// let userNum = prompt("Guess the game num :");
+
+// while (userNum != gameNum) {
+//     userNum = prompt("You entered wrong game num. Try again :");
+// }
+
+// console.log("Congratulation you have entered the right num :");
+
+//strings in js
+
+// let str = "Sangita Magar";
+
+// console.log(str.length); //13
+// console.log(str[11]); //a
+
+//templete literals
+
+// let specialString = `This is a special string`;
+// console.log(specialString);
+
+// let obj = {
+//     item: "pen",
+//     price: 10
+// };
+
+// console.log("The price of the", obj.item, "is", obj.price, "rupees");
+
+// console.log(`The price of the ${obj.item} is ${obj.price} rupees `);
+
+//practice Q1
+// let fullName = prompt("Enter you full name without spaces :");
+
+// let userName = "@" + fullName + fullName.length;
+
+// console.log(userName);
+
---
+++ script.js
@@ -0,0 +1,152 @@
+//Arithmetic operators
+// let a = 5;
+// let b = 2;
+//  console.log("a =",a,"& b =", b);
+// console.log("a+b =", a+b); //7
+// console.log("a-b =", a-b); //3
+// console.log("a*b =", a*b); //10
+// console.log("a/b =", a/b); //2.5
+// console.log("a%b =", a%b); //1
+// console.log("a**b =", a**b); //25
+
+// //Unary Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a,"& b = ", b);
+// a--;
+// console.log("a = ", a); //4
+
+//Assignment Operators
+// let a = 5;
+// let b = 2;
+
+// a **= 4
+// console.log("a =", a); //625
+
+//comparison ooperators
+// let a = 5;
+// let b = 5;
+
+// console.log("a >= b", a >= b); //true
+
+//Logical operators
+// let a = 5;
+// let b = 4;
+//console.log("a && b", a === b && a === b ); //true
+//console.log("a && b", a === b && a !== b ); //false
+
+// console.log("a || b", a > b || a < b ); //true
+
+// console.log("!(a > b)", !(a > b) ); //fasle
+
+// console.log("!(a < b)", !(a < b) ); //true
+
+//Conditional statement
+//  let mode = "light";
+//  let color;
+
+//  if (mode === "dark") {
+//      color = "black";
+//  };
+
+// if (mode === "light") {
+//     color = "white";
+// };
+
+//  console.log(color); //white
+
+//  let mode = "dark";
+//  let color;
+
+//  if (mode === "dark") {
+//      color = "black";
+//  };
+
+// if (mode === "light") {
+//     color = "white";
+// };
+
+//  console.log(color); //black
+
+// let mode = "light";
+//  let color;
+
+//  if (mode === "dark") {
+//      color = "black";
+//  };
+
+// if (mode === "light") {
+//     color = "white";
+// };
+
+//  console.log(color); //white
+
+//  let mode = "light";
+//  let color;
+
+//  if (mode === "dark") {
+//      color = "black";
+//   } else {
+//     color = "white";
+//   }
+
+//   console.log(color); //white
+
+//odd and even
+// let num = 10
+
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }; //is even
+
+// let num = 7
+
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }; //is odd
+
+
+//Ternary operator
+
+// let age = 21
+// let result = age >= 18 ? "adult" : "not adult"
+
+// console.log(result); //adult
+
+
+//practice set Q1.
+
+// let num = prompt("enter a number:");
+
+// if (num % 5 === 0) {
+//     console.log(num, "is a multipule of 5")
+// } else {
+//     console.log(num, "is not multipule of 5")
+// }
+
+//practice set Q2
+//  let score = prompt("enter your score 0-100:")
+//  let grade;
+
+//  if (score >= 90 && score <= 100) {
+//     grade = "A"
+//  } else if (score >= 70 && score <= 89) {
+//     grade = "B"
+//  } else if (score >= 60 && score <= 69) {
+//     grade = "C"
+//  } else if (score >= 50 && score <= 59) {
+//     grade = "D"
+//  } else if (score >= 0 && score <= 49) {
+//     grade = "F"
+//  }
+
+//  console.log(grade); 
+
+//loops in js
+
+// console.log("apna collage");
